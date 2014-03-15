8b8e5ce411e389a6d150687caadd68dad5a14181
Steve4448 <steve4448@gmail.com>
2014-03-15 19:23:12 -0400

    Simple MD5 method.

---
+++ src/net/nexustools/extrafeaturesprovider/util/CryptoHelper.java
@@ -0,0 +1,24 @@
+package net.nexustools.extrafeaturesprovider.util;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class CryptoHelper {
+	
+	//Originally from http://fancifulandroid.blogspot.ca/2014/01/android-convert-string-to-md5-properly.html
+	public static String MD5(String message) {
+		try {
+			MessageDigest messageDigester = MessageDigest.getInstance("MD5");
+			byte[] digested = messageDigester.digest(message.getBytes());
+			
+			StringBuffer stringBuffer = new StringBuffer();
+			for(int i = 0; i < digested.length; i++) 
+				stringBuffer.append(Integer.toHexString((digested[i] & 0xFF) | 0x100).substring(1, 3));
+			
+			return stringBuffer.toString();
+		} catch(NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+}
