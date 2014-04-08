4caa458747d2cf38f22a578918430067177b71f3
Steve4448 <steve4448@gmail.com>
2014-04-07 22:11:29 -0400

    Scale the image based off maxSize.

--- src/net/nexustools/extrafeaturesprovider/util/BitmapHelper.java
+++ src/net/nexustools/extrafeaturesprovider/util/BitmapHelper.java
@@ -2,9 +2,6 @@ package net.nexustools.extrafeaturesprovider.util;
 
 import android.graphics.Bitmap;
 
-/**
- * Such hacky.
- */
 public class BitmapHelper {
 	
 	/**
@@ -13,11 +10,22 @@ public class BitmapHelper {
 	 * @param toOptimize The bitmap to be optimized.
 	 * @param renderer The GL context to use in order to detect the devices maximum texture size.
 	 * @param filter Should the bitmap filter the image when scaled to the limited size?
+	 * @param maxSize The size to limit the images to width & height wise.
 	 * @return The optimized/compatible bitmap.
 	 */
-	public static Bitmap optimizeCompatibility(Bitmap toOptimize, boolean filter) {
+	public static Bitmap optimizeCompatibility(Bitmap toOptimize, boolean filter, int maxSize) {
 		int width = MiscMath.getNearestPowerOfTwo(toOptimize.getWidth());
 		int height = MiscMath.getNearestPowerOfTwo(toOptimize.getHeight());
+		
+		if(width > maxSize || height > maxSize) {
+			int scaledSize = 1;
+			
+			while(height / scaledSize > maxSize || width / scaledSize > maxSize) {
+				scaledSize *= 2;
+			}
+			width /= scaledSize;
+			height /= scaledSize;
+		}
 		return Bitmap.createScaledBitmap(toOptimize, width, height, filter);
 	}
 }
